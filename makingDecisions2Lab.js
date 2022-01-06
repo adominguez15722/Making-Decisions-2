81883a55f693621091202a4a8c69bc9fdd191cab
Anthony <adominguez15722@gmail.com>
2022-01-06 16:54:17 -0600

    Problem 6 is solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -114,6 +114,16 @@ console.log(someNum);
 
 //Code Here
 
+// console.log(myFavoriteNumbers[7])
+// if (myFavoriteNumbers.length < 7) {
+//   console.log('There are not enough elements')
+// } else if (myFavoriteNumbers.length === 7)  {
+//   console.log('There are 7 elements in this array')
+// } else {
+//   myFavoriteNumbers.push(someNum)
+// };
+
+// console.log(myFavoriteNumbers[7])
 
 ////////// PROBLEM 8 //////////
 
@@ -125,6 +135,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+// for (let i = 0; i < listOfNumbers.length; i++) {
+//   if((listOfNumbers[i] % 3) === 0)  {
+//     console.log(`${listOfNumbers[i]} is divisible by 3`)
+//   }
+// }
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
