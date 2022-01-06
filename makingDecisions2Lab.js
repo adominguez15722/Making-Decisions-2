fbfbb3dc0c9710f2d39a6d4f4674b72db7737c8b
Anthony <adominguez15722@gmail.com>
2022-01-06 14:48:56 -0600

    Problem 4 is solved

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -38,9 +38,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 
 //Code Here
 
-for (let i = 0; i < family.length; i++) {
-  console.log(family[i])
-};
+// for (let i = 0; i < family.length; i++) {
+//   console.log(family[i])
+// };
 
 
 ////////// PROBLEM 4 //////////
@@ -55,6 +55,15 @@ let evensArr = []
 */
 
 //Code Here
+
+for (let i = 0; i < nums.length; i++) {
+  if ((nums[i] % 2) === 0){
+    evensArr.push(nums[i])
+    console.log(evensArr)
+  }
+}
+
+
 
 
 
